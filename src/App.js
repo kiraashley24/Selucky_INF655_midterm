fd78d2c920a7b1fcd982763cbde7acf5928d97a9
Kira Selucky <kiraashley24@gmail.com>
2024-03-23 21:06:53 -0600

    added header/nav and added ProductDetail

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './pages/Header';
 import HomePage from './pages/HomePage';
 import Search from './components/Search';
 import Cart from './components/Cart';
 import { CartProvider } from './components/context/CartContext';
 import ProductList from './pages/ProductList';
+import ProductDetail from './pages/ProductDetail';
 
 const App = () => {
   return (
     <Router>
       <CartProvider>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/productlist" element={ProductList} />
           <Route path="/search" element={Search} />
           <Route path="/cart" element={Cart} />
+          <Route path="/product/:id" element={<ProductDetail />} />
         </Routes>
       </CartProvider>
     </Router>
